2d9716669041ac6eb6b5b444bef1d8217c90077c
samsoucoupe <64348478+samsoucoupe@users.noreply.github.com>
2024-12-12 21:55:22 +0000

    refactor DiceController to use DiceService for rolling dice

--- dice/src/main/java/miage/M1/dice/DiceController.java
+++ dice/src/main/java/miage/M1/dice/DiceController.java
@@ -1,25 +1,32 @@
 package miage.M1.dice;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class DiceController {
 
+    @Autowired
+    private DiceService diceService;
+
     @GetMapping("/rollDice")
     public int rollDice() {
-        Dice dice = new Dice();
-        dice.roll();
-        return dice.getValue();
+        return diceService.rollDice(1).get(0);
+
     }
 
     @GetMapping("/rollDices/{X}")
     public int[] rollDices(@PathVariable int X) {
-        int[] results = new int[X];
-        for (int i = 0; i < X; i++) {
-            results[i] = rollDice();
+        List<Integer> results = diceService.rollDice(X);
+        int[] resultsArray = new int[results.size()];
+        for (int i = 0; i < results.size(); i++) {
+            resultsArray[i] = results.get(i);
         }
-        return results;
+        return resultsArray;
     }
 }
