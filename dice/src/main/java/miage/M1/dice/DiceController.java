59e6014eccbe98f1451ae77013e12ab0e7913e5e
samsoucoupe <64348478+samsoucoupe@users.noreply.github.com>
2024-12-13 09:25:48 +0000

    add Swagger annotations to DiceController and HistoriquesDiceController for API documentation

--- dice/src/main/java/miage/M1/dice/DiceController.java
+++ dice/src/main/java/miage/M1/dice/DiceController.java
@@ -2,6 +2,12 @@ package miage.M1.dice;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +20,21 @@ public class DiceController {
     @Autowired
     private DiceService diceService;
 
+    @Operation(summary = "Roll a dice")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dice rolled successfully", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/rollDice")
     public int rollDice() {
         return diceService.rollDice(1).get(0);
-
     }
 
+    @Operation(summary = "Roll X dices")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dices rolled successfully", content = @Content(examples = @ExampleObject(value = "[3, 6, 2]"))),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/rollDices/{X}")
     public int[] rollDices(@PathVariable int X) {
         List<Integer> results = diceService.rollDice(X);
--- dice/src/main/java/miage/M1/dice/HistoriquesDiceController.java
+++ dice/src/main/java/miage/M1/dice/HistoriquesDiceController.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.util.List;
 
 @RestController
@@ -13,6 +18,11 @@ public class HistoriquesDiceController {
     @Autowired
     private DiceRepository diceRepository;
 
+    @Operation(summary = "Get all dice roll logs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dice roll logs retrieved successfully", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public List<DiceRollLog> getDiceRollLogs() {
         return diceRepository.findAll();
