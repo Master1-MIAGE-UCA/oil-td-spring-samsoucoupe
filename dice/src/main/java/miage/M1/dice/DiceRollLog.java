087a65e34fa5d3316c265516482f621e3595c9c6
samsoucoupe <64348478+samsoucoupe@users.noreply.github.com>
2024-12-13 10:21:12 +0000

    remove redundant getters and setters from DiceRollLog class

--- dice/src/main/java/miage/M1/dice/DiceRollLog.java
+++ dice/src/main/java/miage/M1/dice/DiceRollLog.java
@@ -26,37 +26,4 @@ public class DiceRollLog {
 
     private LocalDateTime timestamp;
 
-    // Getters and setters
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getDiceCount() {
-        return diceCount;
-    }
-
-    public void setDiceCount(int diceCount) {
-        this.diceCount = diceCount;
-    }
-
-    public List<Integer> getResults() {
-        return results;
-    }
-
-    public void setResults(List<Integer> results) {
-        this.results = results;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
 }
